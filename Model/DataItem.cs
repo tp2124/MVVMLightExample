6814b5ecb147e3facdd036f301f67e7bb6619714
tp2124 <tp2124@gmail.com>
2018-08-14 00:11:24 -0700

    Initial commit of WPF project stubbed in.

---
+++ Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace MVVMLightExample.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
